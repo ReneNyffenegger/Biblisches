9adefe57d2f2f0f07f16541487d658240551ab81
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-07-31 07:33:05 +0200

    Flut Beginn, 120 Jahre vor der Flut

--- Chronologie/AdamBisJosef.pl
+++ Chronologie/AdamBisJosef.pl
@@ -3,6 +3,10 @@ use strict;
 
 use BiblischeChronologie;
 
-print "Jahr der Entrükcung Henochs:   " . BiblischeChronologie::Jahr('Henoch entrückt') . "\n";
-print "Jahr des Todes von Lamech:     " . BiblischeChronologie::Jahr('Lamech✝'        ) . "\n";
-print "Jahr des Todes von Methusalah: " . BiblischeChronologie::Jahr('Methusalah✝'    ) . "\n";
+print "Jahr der Entrükcung Henochs:   " . BiblischeChronologie::Jahr('Henoch entrückt'   ) . "\n";
+print "Jahr des Todes von Lamech:     " . BiblischeChronologie::Jahr('Lamech†'           ) . "\n";
+print "Jahr des Todes von Methusalah: " . BiblischeChronologie::Jahr('Methusalah†'       ) . "\n";
+print "Jahr des Flutanfangs:          " . BiblischeChronologie::Jahr('Flut Beginn'       ) . "\n";
+print "Jahr 120 vor Flutanfang:       " . BiblischeChronologie::Jahr('120 Jahre vor Flut') / 2 . "\n";
+print "Mitte Henoch:                  " .(BiblischeChronologie::Jahr('Henoch*'           ) +
+                                          BiblischeChronologie::Jahr('Henoch entrückt'   )) / 2 . "\n";
--- Chronologie/BiblischeChronologie.pm
+++ Chronologie/BiblischeChronologie.pm
@@ -20,16 +20,19 @@ sub Ereignis { # {{{
   if (my $nach_ereignis = $opts{nach}) {
 
     die "Ereignis $ereignis_name kann nicht bestimmt werden, da Ereignis $nach_ereignis nicht bekannt ist" if not exists $Ereignisse{$nach_ereignis};
-
     die "Bei Ereignis $ereignis_name wurden die Jahre nicht angegeben" unless $opts{jahre};
 
-
     $Ereignisse{$ereignis_name}{jahr} = $Ereignisse{$nach_ereignis}{jahr} + $opts{jahre};
+  }
+  elsif (my $vor_ereignis = $opts{vor}) {
 
+    die "Ereignis $ereignis_name kann nicht bestimmt werden, da Ereignis $vor_ereignis nicht bekannt ist" if not exists $Ereignisse{$vor_ereignis};
+    die "Bei Ereignis $ereignis_name wurden die Jahre nicht angegeben" unless $opts{jahre};
 
+    $Ereignisse{$ereignis_name}{jahr} = $Ereignisse{$vor_ereignis}{jahr} - $opts{jahre};
   }
   else {
-    die "Option 'nach' für Ereignis $ereignis_name nicht angegeben\n";
+    die "weder Option 'nach' noch 'vor' für Ereignis $ereignis_name wurde angegeben\n";
   }
 
 
@@ -68,33 +71,36 @@ BEGIN { # {{{
 
    $Ereignisse{'Adam*'}{jahr} = 0;
 
-   Ereignis('Seth*'                    , nach=>'Adam*'                 , jahre=>  130, vers=>'1. Mo 5:1' );
-   Ereignis('Enosch*'                  , nach=>'Seth*'                 , jahre=>  105, vers=>'1. Mo 5:6' );
-   Ereignis('Adam✝'                    , nach=>'Seth*'                 , jahre=>  903, vers=>'1. Mo 5:5' );
-   Ereignis('Seth✝'                    , nach=>'Seth*'                 , jahre=>  912, vers=>'1. Mo 5:8' );
-   Ereignis('Kenan*'                   , nach=>'Enosch*'               , jahre=>   90, vers=>'1. Mo 5:9' );
-   Ereignis('Enosch✝'                  , nach=>'Enosch*'               , jahre=>  905, vers=>'1. Mo 5:11');
-   Ereignis('Mahalaleel*'              , nach=>'Kenan*'                , jahre=>   70, vers=>'1. Mo 5:12');
-   Ereignis('Jared*'                   , nach=>'Mahalaleel*'           , jahre=>   65, vers=>'1. Mo 5:15');
-   Ereignis('Kenan✝'                   , nach=>'Kenan*'                , jahre=>  910, vers=>'1. Mo 5:14');
-   Ereignis('Mahalaleel✝'              , nach=>'Mahalaleel*'           , jahre=>  895, vers=>'1. Mo 5:17');
-   Ereignis('Henoch*'                  , nach=>'Jared*'                , jahre=>  162, vers=>'1. Mo 5:18');
-   Ereignis('Jared✝'                   , nach=>'Jared*'                , jahre=>  962, vers=>'1. Mo 5:20');
-   Ereignis('Methusalah*'              , nach=>'Henoch*'               , jahre=>   65, vers=>'1. Mo 5:21');
-   Ereignis('Henoch entrückt'          , nach=>'Henoch*'               , jahre=>  365, vers=>'1. Mo 5:23');
-   Ereignis('Lamech*'                  , nach=>'Methusalah*'           , jahre=>  187, vers=>'1. Mo 5:25');
-   Ereignis('Lamech✝'                  , nach=>'Lamech*'               , jahre=>  777, vers=>'1. Mo 5:31');
-   Ereignis('Methusalah✝'              , nach=>'Methusalah*'           , jahre=>  969, vers=>'1. Mo 5:27');
+   Ereignis('Seth*'                    , nach=>'Adam*'                 , jahre=>  130, vers=>'1. Mo 5:1'         );
+   Ereignis('Enosch*'                  , nach=>'Seth*'                 , jahre=>  105, vers=>'1. Mo 5:6'         );
+   Ereignis('Adam†'                    , nach=>'Seth*'                 , jahre=>  903, vers=>'1. Mo 5:5'         );
+   Ereignis('Seth†'                    , nach=>'Seth*'                 , jahre=>  912, vers=>'1. Mo 5:8'         );
+   Ereignis('Kenan*'                   , nach=>'Enosch*'               , jahre=>   90, vers=>'1. Mo 5:9'         );
+   Ereignis('Enosch†'                  , nach=>'Enosch*'               , jahre=>  905, vers=>'1. Mo 5:11'        );
+   Ereignis('Mahalaleel*'              , nach=>'Kenan*'                , jahre=>   70, vers=>'1. Mo 5:12'        );
+   Ereignis('Jared*'                   , nach=>'Mahalaleel*'           , jahre=>   65, vers=>'1. Mo 5:15'        );
+   Ereignis('Kenan†'                   , nach=>'Kenan*'                , jahre=>  910, vers=>'1. Mo 5:14'        );
+   Ereignis('Mahalaleel†'              , nach=>'Mahalaleel*'           , jahre=>  895, vers=>'1. Mo 5:17'        );
+   Ereignis('Henoch*'                  , nach=>'Jared*'                , jahre=>  162, vers=>'1. Mo 5:18'        );
+   Ereignis('Jared†'                   , nach=>'Jared*'                , jahre=>  962, vers=>'1. Mo 5:20'        );
+   Ereignis('Methusalah*'              , nach=>'Henoch*'               , jahre=>   65, vers=>'1. Mo 5:21'        );
+   Ereignis('Henoch entrückt'          , nach=>'Henoch*'               , jahre=>  365, vers=>'1. Mo 5:23'        );
+   Ereignis('Lamech*'                  , nach=>'Methusalah*'           , jahre=>  187, vers=>'1. Mo 5:25'        );
+   Ereignis('Lamech†'                  , nach=>'Lamech*'               , jahre=>  777, vers=>'1. Mo 5:31'        );
+   Ereignis('Methusalah†'              , nach=>'Methusalah*'           , jahre=>  969, vers=>'1. Mo 5:27'        );
+   Ereignis('Noah*'                    , nach=>'Lamech*'               , jahre=>  182, vers=>'1. Mo 5:28-29'     );
+   Ereignis('Flut Beginn'              , nach=>'Noah*'                 , jahre=>  600, vers=>'1. Mo 7:6'         );
+   Ereignis('120 Jahre vor Flut'       , vor =>'Flut Beginn'           , jahre=>  120, vers=>'1. Mo 6:3'         );
 
-   Zeitspanne('Adam',                  , von=>'Adam*'                  , bis=>'Adam✝'                  );
-   Zeitspanne('Seth',                  , von=>'Seth*'                  , bis=>'Seth✝'                  );
-   Zeitspanne('Enosch'                 , von=>'Enosch*'                , bis=>'Enosch✝'                );
-   Zeitspanne('Kenan'                  , von=>'Kenan*'                 , bis=>'Kenan✝'                 );
-   Zeitspanne('Mahalaleel'             , von=>'Mahalaleel*'            , bis=>'Mahalaleel✝'            );
-   Zeitspanne('Jared'                  , von=>'Jared*'                 , bis=>'Jared✝'                 );
+   Zeitspanne('Adam',                  , von=>'Adam*'                  , bis=>'Adam†'                  );
+   Zeitspanne('Seth',                  , von=>'Seth*'                  , bis=>'Seth†'                  );
+   Zeitspanne('Enosch'                 , von=>'Enosch*'                , bis=>'Enosch†'                );
+   Zeitspanne('Kenan'                  , von=>'Kenan*'                 , bis=>'Kenan†'                 );
+   Zeitspanne('Mahalaleel'             , von=>'Mahalaleel*'            , bis=>'Mahalaleel†'            );
+   Zeitspanne('Jared'                  , von=>'Jared*'                 , bis=>'Jared†'                 );
    Zeitspanne('Henoch'                 , von=>'Henoch*'                , bis=>'Henoch entrückt'        );
-   Zeitspanne('Methusalah'             , von=>'Methusalah*'            , bis=>'Methusalah✝'            );
-   Zeitspanne('Lamech'                 , von=>'Lamech*'                , bis=>'Lamech✝'                );
+   Zeitspanne('Methusalah'             , von=>'Methusalah*'            , bis=>'Methusalah†'            );
+   Zeitspanne('Lamech'                 , von=>'Lamech*'                , bis=>'Lamech†'                );
 
 
 } # }}}
